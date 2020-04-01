9ed4a6867da5b8220def41d17ddd3ea25a067806
James <gobot1234yt@gmail.com>
2020-04-01 15:19:58 +0100

    Add some tighter integration for items

--- pricestf/pricestf.py
+++ pricestf/pricestf.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 from json import load
 from enum import IntEnum
 from typing import Optional
@@ -10,6 +11,22 @@ session = aiohttp.ClientSession()
 with open('defindex_mapping.json') as f:
     mapping = load(f)
     f.close()
+
+
+class Price:
+    def __init__(self, price):
+        self.keys = price['keys']
+        self.metal = price['metal']
+
+
+class Item:
+    def __init__(self, data):
+        self.name = data['name']
+        self.sku = data['sku']
+        self.source = data['source']
+        self.last_updated = datetime.utcfromtimestamp(data['time'])
+        self.buy_price = Price(data['buy'])
+        self.sell_price = Price(data['sell'])
 
 
 class Quality(IntEnum):
@@ -31,9 +48,9 @@ class Quality(IntEnum):
 
 
 async def request(defindex, *, quality: Quality = Quality.Unique,
-                  australium: Optional[str] = None, killstreak: Optional[str] = None) -> aiohttp.ClientResponse:
-    australium = australium if australium else ""
-    killstreak = killstreak if killstreak else ""
+                  australium: Optional[str] = None, killstreak: Optional[str] = None) -> dict:
+    australium = australium if australium else ''
+    killstreak = killstreak if killstreak else ''
     url = f"https://api.prices.tf/items/{defindex};{quality}{australium}{killstreak}?src=bptf"
 
     resp = await session.get(url)
@@ -41,9 +58,10 @@ async def request(defindex, *, quality: Quality = Quality.Unique,
         return await resp.json()
     elif resp.status == 429:
         await asyncio.sleep(float(resp.headers['Retry-After']))
+        await request(defindex, quality=quality, australium=australium, killstreak=killstreak)
 
 
-async def get_price(name, *, quality: Quality = Quality.Unique, australium: bool = False, killstreak: int = 0) -> dict:
+async def get_price(name, *, quality: Quality = Quality.Unique, australium: bool = False, killstreak: int = 0) -> Item:
     if australium:
         australium = ";australium"
     if 0 < killstreak <= 3:
@@ -54,14 +72,9 @@ async def get_price(name, *, quality: Quality = Quality.Unique, australium: bool
     except KeyError:
         raise
 
-    content = await request(defindex, quality=quality, australium=australium, killstreak=killstreak)
+    data = await request(defindex, quality=quality, australium=australium, killstreak=killstreak)
 
-    if not content['success']:
-        raise aiohttp.ClientError(content['message'])
-    data = {
-        "name": content['name'],
-        "buy_price": content['buy'],
-        "sell_price": content['sell']
-    }
+    if not data['success']:
+        raise aiohttp.ClientError(data['message'])
 
-    return data
+    return Item(data)
